7b3db80e662fe32b25fb7c7c1e194e525949135a
jyywong <wong.jonathan1@gmail.com>
2021-09-05 20:59:11 -0400

    Added inventory animation

--- src/components/Project2.jsx
+++ src/components/Project2.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { StaticImage } from 'gatsby-plugin-image';
 import { FaGithub } from 'react-icons/fa';
+import Matter, { Engine, Render, World, Bodies, MouseConstraint } from 'matter-js';
 
 import Layout from './Layout';
 import styled from 'styled-components';
 import LiveButton from './blocks/LiveButton';
+import { render } from 'react-dom';
+
+const STATIC_DENSITY = 15;
 
 const Background = styled.div`
 	position: relative;
@@ -17,6 +21,11 @@ const Background = styled.div`
 	align-items: center;
 	justify-content: flex-end;
 	scroll-snap-align: start;
+`;
+const PhysicsDiv = styled.div`
+	position: absolute;
+	width: 100%;
+	height: 100%;
 `;
 
 const DescriptionDiv = styled.div`
@@ -115,6 +124,25 @@ const BackPolygon = styled(motion.div)`
 `;
 
 const Project2 = ({ project2Animate }) => {
+	const boxRef = useRef(null);
+	const canvasRef = useRef(null);
+
+	const [ constraints, setConstraints ] = useState();
+	const [ scene, setScene ] = useState();
+
+	const handleResize = () => {
+		setConstraints(boxRef.current.getBoundingClientRect());
+	};
+	const handleClick = () => {
+		console.log('click');
+		const mouseX = scene.engine.world.constraints[0].pointA.x;
+		const mouseY = scene.engine.world.constraints[0].pointA.y;
+		const randomWidth = Math.floor(Math.random() * (120 - 75 + 1) + 75);
+		const randomHeight = Math.floor(Math.random() * (120 - 75 + 1) + 75);
+
+		World.add(scene.engine.world, Bodies.rectangle(mouseX, mouseY, randomWidth, randomHeight));
+	};
+
 	const controlTitle = useAnimation();
 	const controlDescription = useAnimation();
 	const controlBackPoly = useAnimation();
@@ -146,6 +174,7 @@ const Project2 = ({ project2Animate }) => {
 		});
 		polyAnimateSeq();
 	};
+
 	useEffect(
 		() => {
 			if (project2Animate) {
@@ -155,9 +184,108 @@ const Project2 = ({ project2Animate }) => {
 		},
 		[ project2Animate ]
 	);
+	useEffect(
+		() => {
+			let Engine = Matter.Engine;
+			let Render = Matter.Render;
+			let World = Matter.World;
+			let Bodies = Matter.Bodies;
+
+			let engine = Engine.create({});
+
+			let render = Render.create({
+				element: boxRef.current,
+				engine: engine,
+				canvas: canvasRef.current,
+				options: {
+					width: window.innerWidth,
+					height: window.innerHeight,
+					background: 'transparent',
+					wireframes: false
+				}
+			});
+
+			const floor = Bodies.rectangle(150, 200, 300, 20, {
+				isStatic: true,
+				render: {
+					fillStyle: 'transparent'
+				}
+			});
+
+			const randomBox = Bodies.rectangle(1200, 0, 75, 75, {
+				render: {
+					fillStyle: 'brown'
+				},
+				angle: Math.PI * 0.1
+			});
+			const randomBox2 = Bodies.rectangle(1250, -1200, 80, 80, {
+				render: {
+					fillStyle: 'yellow'
+				},
+				angle: Math.PI * 0.2
+			});
+			const randomBox3 = Bodies.rectangle(1300, -600, 90, 90, {
+				render: {
+					fillStyle: 'grey'
+				},
+				angle: Math.PI * 0.15
+			});
+
+			const mConstraint = MouseConstraint.create(engine);
+
+			World.add(engine.world, [ floor, randomBox, randomBox2, randomBox3, mConstraint ]);
+
+			Engine.run(engine);
+			Render.run(render);
+			setConstraints(boxRef.current.getBoundingClientRect());
+			setScene(render);
+			window.addEventListener('resize', handleResize);
+
+			return () => {
+				window.removeEventListener('resize', handleResize);
+			};
+		},
+		[ project2Animate ]
+	);
+
+	useEffect(
+		() => {
+			if (constraints) {
+				let { width, height } = constraints;
+
+				// Dynamically update canvas and bounds
+				scene.bounds.max.x = width;
+				scene.bounds.max.y = height;
+				scene.options.width = width;
+				scene.options.height = height;
+				scene.canvas.width = width;
+				scene.canvas.height = height;
+
+				// Dynamically update floor
+				const floor = scene.engine.world.bodies[0];
+
+				Matter.Body.setPosition(floor, {
+					x: width / 2,
+					y: height + STATIC_DENSITY / 2 - 100
+				});
+
+				Matter.Body.setVertices(floor, [
+					{ x: 0, y: height },
+					{ x: width, y: height },
+					{ x: width, y: height + STATIC_DENSITY },
+					{ x: 0, y: height + STATIC_DENSITY }
+				]);
+			}
+		},
+		[ scene, constraints ]
+	);
+
 	return (
 		<Layout>
-			<Background className="Project2">
+			<Background className="Project2" onClick={handleClick}>
+				<PhysicsDiv ref={boxRef}>
+					<canvas ref={canvasRef} />
+				</PhysicsDiv>
 				<DescriptionDiv>
 					<ProjectTitle animate={controlTitle} initial={{ opacity: 0 }}>
 						Project Name
